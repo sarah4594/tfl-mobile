23170d7b5f98a4c3c2f608fbc9c6c7004297cc25
Sarah CJ <sarah4594@gmail.com>
2024-04-13 14:32:44 -0500

    Setup root layout with title

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,7 +10,7 @@ export const unstable_settings = {
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: true, title: 'Tickets For Less' }} />
       <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
     </Stack>
   );
