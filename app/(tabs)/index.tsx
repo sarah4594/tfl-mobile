189312b630326a0c343114ea42eae3e25dcc045d
Sarah CJ <sarah4594@gmail.com>
2024-04-13 15:01:32 -0500

    Add card scrollview

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,20 @@
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '#components/ScreenContent';
+import { View, Text, ScrollView } from 'react-native';
 
 export default function Home() {
   return (
     <>
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Home" />
-      </View>
+      <ScrollView className="flex-1">
+        <Text className="mx-4 mt-4 text-2xl font-bold text-gray-800">Kansas City, MO</Text>
+        <View className="mt-6">
+          <Text className="mx-4 text-xl font-bold text-gray-800">Sports Near Kansas City</Text>
+          <ScrollView className="mt-4 flex" horizontal>
+            <View className="ml-4 h-40 w-40 flex-none rounded-lg bg-gray-200" />
+            <View className="ml-4 h-40 w-40 flex-none rounded-lg bg-gray-200" />
+            <View className="ml-4 h-40 w-40 flex-none rounded-lg bg-gray-200" />
+            <View className="ml-4 mr-4 h-40 w-40 flex-none rounded-lg bg-gray-200" />
+          </ScrollView>
+        </View>
+      </ScrollView>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
